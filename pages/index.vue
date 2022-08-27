888d44d3ab627c7dd3b7d2599799c0551dcd3255
张长生 <97137180+20220105@users.noreply.github.com>
2022-08-28 01:04:06 +0800

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <template>
+	<!-- 自动刷新 -->
+	<!-- <meta http-equiv="refresh" content="5"> -->
 	<view class="box">
 
 		<view class="index">
@@ -8,21 +10,22 @@
 			<view class="index_two">
 				一 一 &nbsp; &nbsp;&lt; &lt; 东汉·诫子书 &gt; &gt;
 			</view>
-			<view class="Tips" v-show="Tips.if">
-				{{Tips.text}}
+
+			<view class="Tips" v-show="Tips">
+				{{text}}
 			</view>
 			<view class="zhiling">
 				<view class="anhao">
-					<image src="../static/security.svg" mode=""></image>
+					<image src="../static/security.svg" mode="" ></image>
 				</view>
-				<input type="number" :placeholder="input.placeholder" v-model="input.value" />
+				<input type="password" :placeholder="input.placeholder" v-model="input.value" />
 				<view class="button">
 					<!-- :disabled="input.value==9527?false:true" -->
 					<button type="default" @click="goIndex()"> 进入书摘 </button>
 				</view>
 			</view>
-
 		</view>
+
 	</view>
 </template>
 
@@ -30,10 +33,10 @@
 	export default {
 		data() {
 			return {
-				Tips:{
-					if:false,
-					text:'请输入正确的口令 ！'
-				},
+
+				Tips: false,
+				text: '请输入正确的口令 ！',
+
 				input: {
 					placeholder: '请输入今夜口令',
 				},
@@ -42,14 +45,22 @@
 		methods: {
 			goIndex() {
 				let a = this.input.value;
+				let b = 9090;
 				// 不能0开头
-				if (a == 20220827) {
+				if (a == b) {
 					uni.navigateTo({
 						url: '././index/index',
-
 					});
 				} else {
-				this.Tips.if=true
+					this.Tips = true,
+				    this.input = '',
+					// 立即刷新页面
+					// location.reload();
+					
+					// 延迟刷新
+					window.setTimeout(function () {
+					     window.location.reload();
+					},2000)
 				};
 				this.input = ''
 			}
@@ -58,16 +69,18 @@
 </script>
 
 <style scoped>
-	.Tips{
+	.Tips {
 		color: red;
-		top: 1150rpx;
-		left: 137rpx;
+		top: 1170rpx;
+		left: 157rpx;
 		position: fixed;
 		font-size: 32rpx;
 	}
+
 	image {
 		width: 90rpx;
 		height: 70rpx;
+		margin-right: 20rpx	;
 	}
 
 	input {
@@ -77,30 +90,35 @@
 		height: 70rpx;
 		border-radius: 0rpx 10rpx 10rpx 0rpx;
 	}
+
 	.zhiling {
 		display: flex;
 		margin: 430rpx 0rpx;
 		padding: 0;
 	}
+
 	button {
 		border-radius: 10rpx;
 		border: 3rpx solid black;
-		color:white;
+		color: white;
 		background-color: black;
 		height: 70rpx;
 		line-height: 70rpx;
 		width: 250rpx;
 	}
+
 	.button {
 		width: 43%;
 		font-family: "楷书";
 		margin-left: 5%;
 	}
+
 	.index_two {
 		text-align: right;
 		margin-top: 20%;
 		font-size: 35rpx;
 	}
+
 	.index {
 		margin-top: 49%;
 		padding: 7%;
@@ -109,6 +127,7 @@
 		font-weight: 900;
 		font-family: "仿宋";
 	}
+
 	.box {
 		top: 0;
 		bottom: 0;
