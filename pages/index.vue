00fcb97bede7073d093a41d4b0687697811e6fb6
张长生 <97137180+20220105@users.noreply.github.com>
2022-08-28 00:07:16 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="box">
-	
+
 		<view class="index">
 			<view class="index_one">
 				非博学无以广才，非志无以成学。
@@ -8,15 +8,20 @@
 			<view class="index_two">
 				一 一 &nbsp; &nbsp;&lt; &lt; 东汉·诫子书 &gt; &gt;
 			</view>
+			<view class="Tips" v-show="Tips.if">
+				{{Tips.text}}
+			</view>
 			<view class="zhiling">
 				<view class="anhao">
-					今夜口令：
+					<image src="../static/security.svg" mode=""></image>
 				</view>
-				<input type="number"  :placeholder="input.placeholder"  v-model="input.value" />
+				<input type="number" :placeholder="input.placeholder" v-model="input.value" />
+				<view class="button">
+					<!-- :disabled="input.value==9527?false:true" -->
+					<button type="default" @click="goIndex()"> 进入书摘 </button>
+				</view>
 			</view>
-			<view class="button">
-			<button type="default"  @click="goIndex()" :disabled="input.value==9527?false:true">	  进入书摘 </button>
-			</view>
+
 		</view>
 	</view>
 </template>
@@ -25,76 +30,77 @@
 	export default {
 		data() {
 			return {
-				input: {
-					placeholder: '芝麻开门',
+				Tips:{
+					if:false,
+					text:'请输入正确的口令 ！'
 				},
-			
+				input: {
+					placeholder: '请输入今夜口令',
+				},
 			};
-			
-
 		},
 		methods: {
-			goIndex(){
-			uni.navigateTo({
-			 url: '././index/index'
-			});
+			goIndex() {
+				let a = this.input.value;
+				// 不能0开头
+				if (a == 20220827) {
+					uni.navigateTo({
+						url: '././index/index',
+
+					});
+				} else {
+				this.Tips.if=true
+				};
+				this.input = ''
 			}
 		}
 	}
 </script>
 
 <style scoped>
-	.jinru {
-		width: 100%;
-		height: 100%;
+	.Tips{
+		color: red;
+		top: 1150rpx;
+		left: 137rpx;
+		position: fixed;
+		font-size: 32rpx;
+	}
+	image {
+		width: 90rpx;
+		height: 70rpx;
 	}
 
-	.anhao {
-		background-color: antiquewhite;
-		text-align: center;
-		line-height: 50rpx;
-		line-height: 50rpx;
-		font-family: "楷书";
-		font-size: 30rpx;
-		width: 200rpx;
-		padding-top: 10rpx;
-
-	}
 	input {
 		border: 2rpx solid black;
 		width: 300rpx;
 		text-align: center;
 		height: 70rpx;
-
+		border-radius: 0rpx 10rpx 10rpx 0rpx;
 	}
-
 	.zhiling {
 		display: flex;
-		margin: 90rpx 30rpx;
+		margin: 430rpx 0rpx;
+		padding: 0;
 	}
-    button{
+	button {
 		border-radius: 10rpx;
-		border: 3rpx solid  black;
-	}
-	button:hover{
-
+		border: 3rpx solid black;
+		color:white;
+		background-color: black;
+		height: 70rpx;
+		line-height: 70rpx;
+		width: 250rpx;
 	}
 	.button {
-		margin-top: 39%;
-		padding: 5%;
 		width: 43%;
-		margin-left: 30%;
-		border-radius: 20rpx;
 		font-family: "楷书";
+		margin-left: 5%;
 	}
-
 	.index_two {
 		text-align: right;
 		margin-top: 20%;
 		font-size: 35rpx;
-		
 	}
-
 	.index {
 		margin-top: 49%;
 		padding: 7%;
@@ -103,7 +109,6 @@
 		font-weight: 900;
 		font-family: "仿宋";
 	}
-
 	.box {
 		top: 0;
 		bottom: 0;
